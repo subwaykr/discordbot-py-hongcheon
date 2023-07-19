32f995d491db297baa3e453cdf45b2a4ebe7cb65
subwaykr <126319900+subwaykr@users.noreply.github.com>
2023-07-19 11:20:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,8 @@ from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
 import os
+intents = discord.Intents.all()
+intents.members = True
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -25,6 +27,31 @@ async def on_message(message):
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('Hello!')
 
+    if message.content == f'{PREFIX}서버오픈' :
+        msg = "@everyone 서버가 오픈되었습니다! 서버를 즐겨보세요! 게임 접속 : https://web.roblox.com/games/8147374455/NamMyeon"
+        channel = client.get_channel(878519332135637022)
+        await channel.send(msg)
+
+    if message.content == f'{PREFIX}서버닫음' :
+        msg = "서버가 닫혔습니다! 다음 오픈을 기대해 주세요!"
+        channel = client.get_channel(878519332135637022)
+        await channel.send(msg)
+
+    if message.content == f'{PREFIX}점검실시' :
+        msg = ":tools: 안내봇 점검 및 테스트중입니다! 이용에 착오없으시길 바랍니다."
+        channel = client.get_channel(878519332135637022)
+        await channel.send(msg)
+
+    if message.content == f'{PREFIX}점검완료' :
+        msg = ":white_check_mark: 안내봇 점검이 종료되었습니다!"
+        channel = client.get_channel(878519332135637022)
+        await channel.send(msg)
+
+@client.event
+async def on_member_join(member) :
+    msg = "안녕하세요! 뉴라이프에 오신것을 환영합니다!"
+    channel = client.get_channel(1098618960297087050)
+    await channel.send(msg)
 
 try:
     client.run(TOKEN)
